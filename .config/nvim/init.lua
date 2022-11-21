c99d02f7ae7d709f4e11093f1b4a91738c18278f
Ursescu Ionut <ursescuionut97@gmail.com>
2022-11-21 21:08:43 +0200

    Other NVIM changes
    Known issues:
    Grepper QFWindow will open over nvimtree and it will fail.
    Possession will not save qfxwindows or location lists.
    
    Signed-off-by: Ionut Ursescu <ionut.ursescu@nxp.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,6 +9,12 @@ vim.cmd([[
     set ignorecase
     set smartcase
     set jumpoptions=stack
+    set nowrap
+    set listchars=tab:→\ ,extends:>,precedes:<,space:·
+    set list
+    set scrolloff=10
+    set sidescrolloff=30
+    set sessionoptions-=buffers
 ]])
 
 vim.cmd [[packadd packer.nvim]]
@@ -49,6 +55,11 @@ require('packer').startup(function()
 
     use {"sitiom/nvim-numbertoggle"}
     use 'folke/tokyonight.nvim'
+    use 'simeji/winresizer'
+    use {
+        'jedrzejboczar/possession.nvim',
+        requires = { 'nvim-lua/plenary.nvim' }
+    }
     use 'dstein64/vim-startuptime'
 end)
 
@@ -128,7 +139,7 @@ require("nvim-tree").setup({
 
 -- Nvim Tree sitter config
 require('nvim-treesitter.configs').setup {
-    ensure_installed = { 'c', 'lua', 'cpp', 'python' },
+    ensure_installed = { 'c', 'lua', 'cpp', 'python', 'rust'},
     sync_install = false,
 
     auto_install = true,
@@ -147,7 +158,42 @@ require('nvim-treesitter.configs').setup {
 }
 
 -- Telescope setup
+local actions = require('telescope.actions')
+local action_layout = require('telescope.actions.layout')
 require('telescope').setup {
+    defaults = {
+        dynamic_preview_title = true,
+        vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+            '--trim'
+        },
+        mappings = {
+            i = {
+                -- ['<esc>'] = actions.close,
+                ['<C-u>'] = false,
+                ['<M-p>'] = action_layout.toggle_preview
+            },
+            n = {
+                ['<M-p>'] = action_layout.toggle_preview
+            },
+        },
+        path_display = {
+            truncate = 30,
+        }
+    },
+    pickers = {
+        find_files = {
+            path_display = {
+                'absolute',
+            }
+        }
+    },
     extensions = {
         file_browser = {
             theme = 'dropdown',
@@ -161,14 +207,87 @@ require('telescope').setup {
         }
     }
 }
+
+require('possession').setup {
+    session_dir = '/home/john/.nvim/config/data/possession',
+    silent = false,
+    load_silent = true,
+    debug = false,
+    prompt_no_cr = false,
+    autosave = {
+        current = false,  -- or fun(name): boolean
+        tmp = false,  -- or fun(): boolean
+        tmp_name = 'tmp',
+        on_load = false,
+        on_quit = false,
+    },
+    commands = {
+        save = 'SSave',
+        load = 'SLoad',
+        close = 'SClose',
+        delete = 'SDel',
+        show = 'SShow',
+        list = 'SList',
+        migrate = 'SMigrate',
+    },
+    hooks = {
+        before_save = function(name) return {} end,
+        after_save = function(name, user_data, aborted) end,
+        before_load = function(name, user_data) return user_data end,
+        after_load = function(name, user_data) end,
+    },
+    plugins = {
+        close_windows = {
+            hooks = {'before_save', 'before_load'},
+            preserve_layout = true,  -- or fun(win): boolean
+            match = {
+                floating = true,
+                buftype = {},
+                filetype = {},
+                custom = false,  -- or fun(win): boolean
+            },
+        },
+        --delete_hidden_buffers = false,
+        delete_hidden_buffers = {
+            hooks = {
+                'before_load',
+                 vim.o.sessionoptions:match('buffer') and 'before_save',
+             },
+             force = false,  -- or fun(buf): boolean
+         },
+        nvim_tree = true,
+        tabby = true,
+        delete_buffers = false,
+    },
+}
+
 require('telescope').load_extension 'file_browser'
 require('telescope').load_extension 'fzf'
+require('telescope').load_extension 'possession'
 
 -- Grepper
 vim.g.grepper = {
     tools = {'rg', 'grep'},
     searchreg = 1,
 }
+
+-- https://github.com/mhinz/vim-grepper
+vim.g.grepper = {tools = {'rg', 'grep'}, searchreg = 1}
+vim.cmd(([[
+    aug Grepper
+        au!
+        au User Grepper ++nested %s
+    aug END
+]]):format([[call setqflist([], 'r', {'context': {'bqf': {'pattern_hl': '\%#' . getreg('/')}}})]]))
+
+-- try `gsiw` under word
+vim.cmd([[
+    nmap gs  <plug>(GrepperOperator)
+    xmap gs  <plug>(GrepperOperator)
+]])
+
+-- Winresizer
+vim.g.winresizer_start_key = '<leader>rs'
 
 -- Key mappers
 function map(mode, lhs, rhs, opts)
@@ -183,9 +302,11 @@ map('n', '<leader>ff', ':Telescope find_files<cr>', {})
 map('n', '<leader>fg', ':Telescope live_grep<cr>', {})
 map('n', '<leader>fe', ':Telescope file_browser<cr>', {})
 map('v', '<leader>c', ':OSCYank<cr>', {})
-map('n', '<leader>*', ':Grepper -tool rg -cword -noprompt<cr>', {})
+--map('n', '<leader>*', ':Grepper -tool rg -cword -noprompt<cr>', {})
 map('n', '<leader>n', ':NvimTreeToggle<cr>', {})
 map('v', '<leader>y', '"+y', {})
+map('', '<f12>', ':set list!<cr>', {})
+map('', '<leader>w', ':set wrap!<cr>', {})
 
 -- Color scheme
 vim.cmd [[colorscheme tokyonight-night]]
